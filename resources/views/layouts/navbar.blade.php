c5d345bdb9637444e8ff46ca291a278a660af08d
abdulnafea <66330574+alsa7err90@users.noreply.github.com>
2023-03-08 13:16:23 +0300

    add navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,75 @@
+<nav class="navbar navbar-expand-sm navbar-light" id="neubar">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="/static_files/images/logos/logo_2.png"
+                height="60" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto ">
+
+                <li class="nav-item dropdown active">
+                    <a class="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        لوحة التحكم
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li> 
+                            <a class="dropdown-item" href="#" >
+                                ارسال الدعوات
+                            </a>
+                            <a class="dropdown-item" href="#" >
+                             الدعوات العامة
+                            </a>
+                            <a class="dropdown-item" href="#" >
+                             الألقاب
+                            </a>
+                            <a class="dropdown-item" href="#" >
+                             الفئات
+                            </a>
+                            <a class="dropdown-item" href="#" >
+                             تعديل معلومات الدخول
+                            </a>
+                            <a class="dropdown-item" href="#" >
+                             اضافة موظفين
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                       يوم الحفل
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li> 
+                            <a class="dropdown-item" href="#" >
+                             جميع الدعوات
+                            </a>
+                            <a class="dropdown-item" href="#" >
+                            الكراسي
+                            </a>
+                            <a class="dropdown-item" href="#" >
+                            الكراسي الفارغة
+                            </a>
+                            <a class="dropdown-item" href="#" >
+                             تقارير الكراسي
+                            </a>
+                            
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  mx-2" href="#">qrcode</a>
+                </li>
+                
+                    
+
+            </ul>
+        </div>
+    </div>
+</nav>
